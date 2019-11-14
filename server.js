0b53ec5c4584eb15857c0acfa6f4becd0d495f2c
Ellert Smári Kristbergsson <ellertsmari@gmail.com>
2019-11-14 11:27:32 +0000

    init commit

---
+++ server.js
@@ -0,0 +1,13 @@
+const app = require('express')();
+const PORT = 3000;
+
+const testable = (a, b)=>{
+    return a+b;
+}
+
+
+app.listen(PORT, ()=>{
+    console.log("listening to port:", PORT);
+})
+
+console.log(testable("talan er: ", 8));
